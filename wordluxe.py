bfa09da8a0dcc472db5634708108a63247465fc5
endevium <148771728+endevium@users.noreply.github.com>
2023-10-24 21:20:04 +0800

    Update wordluxe.py
    coins

--- wordluxe.py
+++ wordluxe.py
@@ -1,12 +1,24 @@
 import random
 
-words = ["APPLE", "LEMON", "MELON", "PEACH", "GUAVA", "GRAPE"]
-random_word = random.choice(words)
+easy_words = ["APPLE", "LEMON", "MELON", "PEACH", "GUAVA", "GRAPE"]
+normal_words = ["CLOUD", "BEACH", "MAPLE", "GREED", "SMILE"]
+hard_words = ["ABYSS", "PRISM", "RAVEN", "CHAOS", "CYNIC"]
+extreme_words = ["QUIRK", "XENON", "VIXEN", "OZONE", "EXALT"]
+word_list = {1:easy_words,2:normal_words,3:hard_words,4:extreme_words}
+
+user_input = int(input("Choose difficulty: "))
+if user_input in word_list:
+    random_word = random.choice(word_list[user_input])
+else:
+    print("Not available.")
+
 word_length = 5
 feedback = ""
-tries = 0
+tries = 1
+currency = 0
 
-while feedback != random_word and tries < 6:
+while feedback != random_word and tries < 7:
+    print("Attempt #", tries)
     guess = input().upper()
     feedback = ""
     tries += 1
@@ -17,4 +29,17 @@ while feedback != random_word and tries < 6:
             feedback += guess[i].lower() # lowercase means correct letter but in wrong position
         else:
             feedback += "X" # cross indicates wrong letter
+    
+    #add coins
+    if feedback == random_word:
+        if tries <= 2:
+            currency += 3 # P-TODO: Fix
+        elif tries <= 4:
+            currency += 2
+        else:
+            currency += 1
+            
     print(feedback)
+    
+print("Coins:", currency)
+print("Game over")
