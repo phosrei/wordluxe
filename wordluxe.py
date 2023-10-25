71821020edc48476af3c125cb459ee5b58b6debb
Reiki <phosmiro@gmail.com>
2023-10-25 22:09:36 +0800

    Ignore cache

--- wordluxe.py
+++ wordluxe.py
@@ -1,5 +1,5 @@
 import random
-import time 
+import time
 from nltk.corpus import words
 from termcolor import colored
 from wordbank import categories, difficulty
@@ -23,15 +23,48 @@ msg = ""
 
 def easy_mode():
     attempts = 0
+
     while True:
+
         print(f"Attempt #{attempts + 1}")
         guess = input()
         feedback = ""
+
         while guess.lower() not in dictionary:
             print(f"'{guess}' is not in the English dictionary.")
             guess = input()
 
-        # TODO: Append letter in a list instead to improve performance and remove checking redundancy
+        for i in range(word_length):
+            if guess[i] in random_word[i]:
+                feedback += colored(guess[i], 'green')
+            elif guess[i] in feedback:
+                feedback += colored(guess[i], 'dark_grey')
+            elif guess[i] in random_word:
+                feedback += colored(guess[i], 'yellow')
+            else:
+                feedback += colored(guess[i], 'dark_grey')
+        
+        print(feedback)
+        if guess == random_word:
+            msg = "You won!"
+            break
+        attempts += 1
+    
+    print("Coins: No coins are rewarded in easy mode.")
+    print(msg)
+
+def normal_mode():
+
+    for attempt in range(1,6+1):
+        print(f"Attempt #{attempt}")
+        guess = input()
+        feedback = ""
+        currency = 0
+
+        while guess.lower() not in dictionary:
+            print(f"'{guess}' is not in the English dictionary.")
+            guess = input()
+
         for i in range(word_length):
             if guess[i] in random_word[i]:
                 feedback += colored(guess[i], 'green')
@@ -46,23 +79,14 @@ def easy_mode():
         #add coins
         if guess == random_word:
             msg = "You won!"
+            if attempt <= 2:
+                currency += 3 
+            elif attempt <= 4:
+                currency += 2
+            else:
+                currency += 1
             break
-        attempts += 1
-    
-    print("Coins: No coins are rewarded in easy mode.")
-    print(msg)
-
-def normal_mode():
-    for attempt in range(1,6+1):
-        print(f"Attempt #{attempt}")
-        guess = input()
-        feedback = ""
-        currency = 0
-        while guess.lower() not in dictionary:
-            print(f"'{guess}' is not in the English dictionary.")
-            guess = input()
-
-       
+        elif guess != random_word and attempt == 6:
             msg = "Game over."
             print(f"The word was: {random_word}")
             break
@@ -71,16 +95,17 @@ def normal_mode():
     print(msg)
 
 def hard_mode():
+
     for attempt in range(1,4+1):
         print(f"Attempt #{attempt}")
         guess = input()
         feedback = ""
         currency = 0
+
         while guess.lower() not in dictionary:
             print(f"'{guess}' is not in the English dictionary.")
             guess = input()
 
-        # TODO: Append letter in a list instead to improve performance and remove checking redundancy
         for i in range(word_length):
             if guess[i] in random_word[i]:
                 feedback += colored(guess[i], 'green')
@@ -106,16 +131,17 @@ def hard_mode():
     print(msg)
 
 def extreme_mode():
+
     for attempt in range(1,3+1):
         print(f"Attempt #{attempt}")
         guess = input()
         feedback = ""
         currency = 0
+        
         while guess.lower() not in dictionary:
             print(f"'{guess}' is not in the English dictionary.")
             guess = input()
 
-        # TODO: Append letter in a list instead to improve performance and remove checking redundancy
         for i in range(word_length):
             if guess[i] in random_word[i]:
                 feedback += colored(guess[i], 'green')
