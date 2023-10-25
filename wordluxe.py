ae04f07b7ac31900f3647729227790a16f2641b8
endevium <148771728+endevium@users.noreply.github.com>
2023-10-25 10:08:34 +0800

    Update wordluxe.py

--- wordluxe.py
+++ wordluxe.py
@@ -1,13 +1,12 @@
 import random
 
 def main():
-    # TODO: Re-evaluate
     categories = ["general", "countries", "animals", "fruits", "sports", "artists", "songs"]
     difficulty = {
-        "easy": ["apple", "lemon", "melon", "peach", "guava", "grape"],
-        "normal": ["cloud", "beach", "maple", "greed", "smile"],
-        "hard": ["abyss", "prism", "raven", "chaos", "cynic"],
-        "extreme": ["quirk", "xenon", "vixen", "ozone", "exalt"]
+        "easy": ["here", "book", "cake", "rain", "bird", "fire", "fish", "game", "jump", "kind", "loud", "moon", "nest", "open", "park", "quit", "rest", "star", "time", "view"],
+        "normal": ["apple", "beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "lemon", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"],
+        "hard": ["bright", "yellow", "purple", "hidden", "flavor", "dinner", "banana", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"],
+        "extreme": ["chicken", "musical", "capital", "journey", "freedom", "guitar", "honesty", "victory", "holiday", "history", "hydrate", "illegal", "silence", "justify", "monster", "breathe", "perfect", "nuclear", "quality", "society"]
     }
 
     user_input = input("Choose category: ")
