6e33709e922f8b719b90ca78801e89369a898cde
endevium <148771728+endevium@users.noreply.github.com>
2023-10-25 19:52:02 +0800

    Create wordbank.py

---
+++ wordbank.py
@@ -0,0 +1,7 @@
+categories = ["general", "countries", "animals", "fruits", "sports", "artists", "songs"]
+difficulty = {
+    "easy": ["here", "book", "cake", "rain", "bird", "fire", "fish", "game", "jump", "kind", "loud", "moon", "nest", "open", "park", "quit", "rest", "star", "time", "view"],
+    "normal": ["apple", "beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "lemon", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"],
+    "hard": ["bright", "yellow", "purple", "hidden", "flavor", "dinner", "banana", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"],
+    "extreme": ["chicken", "musical", "capital", "journey", "freedom", "unknown", "honesty", "victory", "holiday", "history", "hydrate", "illegal", "silence", "justify", "monster", "breathe", "perfect", "nuclear", "quality", "society"]
+}
