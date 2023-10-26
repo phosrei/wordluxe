f8ba9e75ddc3f6f90f74be26644718fd64bb066b
Reiki <phosmiro@gmail.com>
2023-10-27 00:58:38 +0800

    Refactor code and remove redundant functions

--- wordluxe.py
+++ wordluxe.py
@@ -14,30 +14,56 @@ wordbank_cat = {
     "songs": wordbank.songs_difficulty
 }
 
-# validates if input is a category and in one of the game modes
-def validate_input(prompt, options):
+def validate_input(prompt, valid_options):
     while True:
         user_input = input(prompt)
-        if user_input in options:
+        if user_input in valid_options:
             return user_input
         else:
             print("Invalid input. Please try again.")
 
-def main():
-    cat_input = validate_input("Choose category: ", wordbank.categories)
-    dif_input = validate_input("Choose difficulty: ", wordbank.gen_difficulty)
+def calculate_reward(attempt, max_attempts):
+    if attempt <= 2:
+        return 3
+    elif attempt <= max_attempts - 2:
+        return 2
+    else:
+        return 1
 
-    word = random.choice(wordbank_cat[cat_input][dif_input])
+def play_game(word, category, max_attempts):
+    currency = 0
+    attempt = 1
 
-    game_modes = {
-    "easy": easy_mode,
-    "normal": normal_mode,
-    "hard": hard_mode,
-    "extreme": extreme_mode
-}
-    game_modes[dif_input](word, cat_input)
+    while attempt <= max_attempts:
+        print(f"Attempt #{attempt}")
+        guess = input()
 
-# check guess and assign a color based on certain conditions
+        if guess.lower() not in dictionary:
+            if category not in ["general", "fruits", "animals"]:
+                break
+            print(f"'{guess}' is not in the English dictionary.")
+            guess = input()
+
+        if len(guess) != len(word):
+            print(f"Guess should be {len(word)} letters.")
+            continue
+
+        print(check_guess(guess, word))
+
+        if guess == word:
+            msg = "You won!"
+            currency += calculate_reward(attempt, max_attempts)
+            break
+        elif guess != word and attempt == max_attempts:
+            msg = "Game over."
+            print(f"The word was: {word}")
+            break
+
+        attempt += 1
+
+    print(f"Coins: {currency}")
+    print(msg)
+
 def check_guess(guess, word):
     length = len(word)
     output = ["-"] * length
@@ -58,122 +84,30 @@ def check_guess(guess, word):
     return ''.join(output)
 
 def easy_mode(word, category):
-    attempts = 1
-
-    while True:
-        print(f"Attempt #{attempts}")
-        guess = input()
-
-        while guess.lower() not in dictionary:
-            if category not in ["general", "fruits", "animals"]:
-                break
-            print(f"'{guess}' is not in the English dictionary.")
-            guess = input()
-    
-        print(check_guess(guess, word))
-
-        if guess == word:
-            msg = "You won!"
-            break
-        attempts += 1
-    
-    print(msg)
-    print("Coins: No coins are rewarded in easy mode.")
+    play_game(word, category, max_attempts=1)
+    print("Coins: No coins are rewarded in easy mode")
 
 def normal_mode(word, category):
-    currency = 0
-    attempt = 1
-
-    while attempt <= 6:
-        print(f"Attempt #{attempt}")
-        guess = input()
-
-        while guess.lower() not in dictionary:
-            if category not in ["general", "fruits", "animals"]:
-                break
-            print(f"'{guess}' is not in the English dictionary.")
-            guess = input()
-
-        print(check_guess(guess, word))
-
-        if guess == word:
-            msg = "You won!"
-            if attempt <= 2:
-                currency += 3 
-            elif attempt <= 4:
-                currency += 2
-            else:
-                currency += 1
-            break
-        elif guess != word and attempt == 6:
-            msg = "Game over."
-            print(f"The word was: {word}")
-            break
-
-        attempt += 1
-
-    print(f"Coins: {currency}")
-    print(msg)
+    play_game(word, category, max_attempts=6)
 
 def hard_mode(word, category):
-    currency = 0
-    attempt = 1
-
-    while attempt <= 4:
-        print(f"Attempt #{attempt}")
-        guess = input()
-
-        while guess.lower() not in dictionary:
-            if category not in ["general", "fruits", "animals"]:
-                break
-            print(f"'{guess}' is not in the English dictionary.")
-            guess = input()
-
-        print(check_guess(guess, word))
-
-        if guess == word:
-            msg = "You won!"
-            if attempt <= 2:
-                currency += 3 
-            elif attempt <= 4:
-                currency += 2
-            else:
-                currency += 1
-            break
-        elif guess != word and attempt == 4:
-            msg = "Game over."
-            print(f"The word was: {word}")
-            break
-    
-    print(f"Coins: {currency}")
-    print(msg)
+    play_game(word, category, max_attempts=4)
 
 def extreme_mode(word, category):
-    currency = 0
-    attempt = 3
-    
-    while attempt <= 3:
-        print(f"Attempt #{attempt}")
-        guess = input()
+    play_game(word, category, max_attempts=3)
 
-        while guess.lower() not in dictionary:
-            if category not in ["general", "fruits", "animals"]:
-                break
-            print(f"'{guess}' is not in the English dictionary.")
-            guess = input()
+def main():
+    cat_input = validate_input("Choose category: ", wordbank.categories)
+    dif_input = validate_input("Choose difficulty: ", wordbank.gen_difficulty)
 
-        print(check_guess(guess, word))
+    word = random.choice(wordbank_cat[cat_input][dif_input])
 
-        if guess == word:
-            msg = "You won!"
-            currency += 10
-            break
-        elif guess != word and attempt == 3:
-            msg = "Game over."
-            print(f"The word was: {word}")
-            break
-    
-    print(f"Coins: {currency}")
-    print(msg)
+    game_modes = {
+        "easy": easy_mode,
+        "normal": normal_mode,
+        "hard": hard_mode,
+        "extreme": extreme_mode
+    }
+    game_modes[dif_input](word, cat_input)
 
-main()
+main
