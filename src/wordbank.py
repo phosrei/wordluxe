78ac38f9df8bbd2a206f2572984d0aa7e8cf9be6
endevium <148771728+endevium@users.noreply.github.com>
2023-10-27 15:56:04 +0800

    Removed words in "general" that exist in "fruits"

--- src/wordbank.py
+++ src/wordbank.py
@@ -1,8 +1,8 @@
 categories = {
     "general": {
         "easy": ["here", "book", "cake", "rain", "bird", "fire", "fish", "game", "jump", "kind", "loud", "moon", "nest", "open", "park", "quit", "rest", "star", "time", "view"],
-        "normal": ["apple", "beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "lemon", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"],
-        "hard": ["bright", "yellow", "purple", "hidden", "flavor", "dinner", "banana", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"],
+        "normal": ["beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"],
+        "hard": ["bright", "yellow", "purple", "hidden", "flavor", "dinner", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"],
         "extreme": ["chicken", "musical", "capital", "journey", "freedom", "unknown", "honesty", "victory", "holiday", "history", "hydrate", "illegal", "silence", "justify", "monster", "breathe", "perfect", "nuclear", "quality", "society"]
     },
     "countries": {
