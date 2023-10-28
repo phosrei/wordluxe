42df5692ea3a39e8ac4578d4bd090df058cf62da
endevium <148771728+endevium@users.noreply.github.com>
2023-10-28 21:49:50 +0800

    Fix indention

--- src/wordluxe.py
+++ src/wordluxe.py
@@ -75,22 +75,24 @@ def play_game(word, category, max_attempts, enable_powerups = True):
 
             if powerup_prompt in ["y", "yes"]:
                 powerup_input = input("Which power up would you like to use? Letter Eraser (LE), Invincibility (IN), or Reveal Vowels (RV) ")
-            elif powerup_input == "LE" and currency >= 1:
-                print('Letter Eraser used. (-1 coin)')
-                eraser_powerup(guess, word)
-                currency -= 1
-            elif powerup_input == "IN" and currency >= 2:
-                print('Invincibilty used. (-2 coins)')
-                currency -= 2
-                continue
-            if powerup_input == "RV" and currency >= 3:
-                print("Reveal Vowels used. (-3 coins)")
-                vowel_powerup(word)
-                currency -= 3
-            else:
-                print('You do not have enough coins')
-        elif powerup_input not in ['n', 'no']:
-            print(f'Invalid input: {powerup_input}')
+                if powerup_input == "LE" and currency >= 1:
+                    print('Letter Eraser used. (-1 coin)')
+                    eraser_powerup(guess, word)
+                    currency -= 1
+                elif powerup_input == "IN" and currency >= 2:
+                    print('Invincibilty used. (-2 coins)')
+                    currency -= 2
+                    continue
+                if powerup_input == "RV" and currency >= 3:
+                    print("Reveal Vowels used. (-3 coins)")
+                    vowel_powerup(word)
+                    currency -= 3
+                else:
+                    print('You do not have enough coins')
+            elif powerup_prompt not in ['n', 'no']:
+                print(f'Invalid input: {powerup_input}')
+        else:
+            pass
 
         attempt += 1
 
