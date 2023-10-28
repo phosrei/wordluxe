68b6abeec283cccd0d801fb7b31583f0182ffedd
endevium <148771728+endevium@users.noreply.github.com>
2023-10-28 21:12:57 +0800

    Added letters

--- src/wordbank.py
+++ src/wordbank.py
@@ -1,44 +1,46 @@
+difficulties = ["easy", "normal", "hard", "extreme"]
+
 categories = {
     "general": {
-        "easy": ["here", "book", "cake", "rain", "bird", "fire", "fish", "game", "jump", "kind", "loud", "moon", "nest", "open", "park", "quit", "rest", "star", "time", "view"],
-        "normal": ["beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"],
-        "hard": ["bright", "yellow", "purple", "hidden", "flavor", "dinner", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"],
-        "extreme": ["chicken", "musical", "capital", "journey", "freedom", "unknown", "honesty", "victory", "holiday", "history", "hydrate", "illegal", "silence", "justify", "monster", "breathe", "perfect", "nuclear", "quality", "society"]
+        "4": ["here", "book", "cake", "rain", "fire", "fish", "game", "jump", "kind", "loud", "moon", "nest", "open", "park", "quit", "rest", "star", "time", "view"],
+        "5": ["beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"],
+        "6": ["bright", "yellow", "purple", "hidden", "flavor", "dinner", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"],
+        "7": ["musical", "capital", "journey", "freedom", "unknown", "honesty", "victory", "holiday", "history", "hydrate", "illegal", "silence", "justify", "monster", "breathe", "perfect", "nuclear", "quality", "society"]
     },
     "countries": {
-        "easy": ["chad", "peru", "oman", "cuba", "togo","fiji", "iran", "mali", "iraq"],
-        "normal": ["egypt", "ghana", "india", "japan", "kenya", "libya", "nepal", "qatar", "spain", "turkey", "yemen", "chile", "bhutan", "haiti", "italy", "libya", "niger", "sudan"],
-        "hard": ["brazil", "canada", "france", "greece", "jordan", "kuwait", "norway", "poland", "russia", "sweden", "turkey"],
-        "extreme": ["andorra", "austria", "bahrain", "belgium", "bolivia", "comoros", "croatia", "eritrea", "estonia", "finland", "georgia", "hungary", "iceland", "lesotho", "moldova", "namibia"]
+        "4": ["chad", "peru", "oman", "cuba", "togo","fiji", "mali", "iraq"],
+        "5": ["egypt", "ghana", "india", "japan", "kenya", "libya", "nepal", "qatar", "spain", "turkey", "yemen", "chile", "bhutan", "haiti", "italy", "libya", "niger", "sudan"],
+        "6": ["brazil", "canada", "france", "greece", "jordan", "kuwait", "norway", "poland", "russia", "sweden", "turkey"],
+        "7": ["andorra", "austria", "bahrain", "belgium", "bolivia", "comoros", "croatia", "eritrea", "estonia", "finland", "georgia", "hungary", "iceland", "lesotho", "moldova", "namibia"]
     },
     "animals": {
-        "easy": ["frog", "duck", "goat", "fish", "crab", "lion", "puma", "hawk", "mule", "hare", "toad", "mole", "lamb"],
-        "normal": ["horse", "zebra", "koala", "lemur", "whale", "gecko", "quail", "rhino", "sloth", "rhino", "hyena", "raven", "shark", "viper"],
-        "hard": ["gibbon", "hornet", "iguana", "jabiru", "jaguar", "ocelot", "toucan", "walrus", "wombat", "spider", "turtle", "mantis"],
-        "extreme": ["cheetah", "giraffe", "gazelle", "penguin", "raccoon", "meerkat", "ostrich", "gorilla", "hamster", "axolotl"]
+        "4": ["frog", "duck", "goat", "fish", "crab", "lion", "puma", "hawk", "mule", "hare", "toad", "mole", "lamb"],
+        "5": ["horse", "zebra", "koala", "lemur", "whale", "gecko", "quail", "rhino", "sloth", "rhino", "hyena", "raven", "shark", "viper"],
+        "6": ["gibbon", "hornet", "iguana", "jabiru", "jaguar", "ocelot", "toucan", "walrus", "wombat", "spider", "turtle", "mantis"],
+        "7": ["cheetah", "giraffe", "gazelle", "penguin", "raccoon", "meerkat", "ostrich", "gorilla", "hamster", "axolotl"]
     },
     "fruits": {
-        "easy": ["plum", "kiwi", "lime", "date", "pear"],
-        "normal": ["apple", "grape", "lemon", "melon", "peach", "guava", "mango"],
-        "hard": ["banana", "cherry", "papaya", "durian", "jujube", "orange", "pomelo"],
-        "extreme": ["avocado", "apricot", "coconut", "kumquat", "currant"]
+        "4": ["plum", "kiwi", "lime", "date", "pear", "corn"],
+        "5": ["apple", "grape", "lemon", "melon", "peach", "guava", "mango"],
+        "6": ["banana", "cherry", "papaya", "durian", "jujube", "orange", "pomelo"],
+        "7": ["avocado", "apricot", "coconut", "kumquat", "currant"]
     },
     "sports": {
-        "easy": ["golf", "judo", "dart", "dive"],
-        "normal": ["board", "dance", "rugby", "chess"],
-        "hard": ["gammon", "cycling", "rowing", "skiing", "diving", "hockey", "karate", "racing"],
-        "extreme": ["surfing", "skating", "cycling", "cricket", "fencing", "hurling", "gliding"]
+        "4": ["golf", "judo", "dart", "dive", "polo", "sumo", "ludo", "putt"],
+        "5": ["board", "dance", "rugby", "chess", "bingo", "poker"],
+        "6": ["gammon", "rowing", "skiing", "diving", "hockey", "karate", "racing"],
+        "7": ["surfing", "skating", "cycling", "cricket", "fencing", "hurling", "gliding", "bowling", "pinball"]
     },
     "artists": {
-        "easy": ["pink", "cher", "muse", "abba", "niki", "doja"],
-        "normal": ["elton", "elvis", "miley", "adele", "twice", "avril"],
-        "hard": ["shakira", "taylor", "eminem", "halsey", "ariana", "olivia"],
-        "extreme": ["britney", "whitney", "shakira", "rihanna", "madonna", "beyonce"]
+        "4": ["pink", "cher", "muse", "abba", "niki", "doja", "itzy"],
+        "5": ["elton", "elvis", "miley", "adele", "twice", "avril"],
+        "6": ["shakira", "taylor", "eminem", "halsey", "ariana", "olivia"],
+        "7": ["britney", "whitney", "shakira", "rihanna", "madonna", "beyonce"]
     },
     "songs": {
-        "easy": ["iris", "halo", "stay", "ymca", "hurt", "burn", "numb"],
-        "normal": ["lover", "ditto", "bones", "toxic", "faded", "hello"],
-        "hard": ["august", "cookie", "signal", "lovely", "royals"],
-        "extreme": ["pompeii", "flowers", "shallow", "finesse", "perfect", "animals", "creepin"]
+        "4": ["iris", "halo", "stay", "ymca", "hurt", "burn", "numb"],
+        "5": ["lover", "ditto", "bones", "toxic", "faded", "hello"],
+        "6": ["august", "cookie", "signal", "lovely", "royals"],
+        "7": ["pompeii", "flowers", "shallow", "finesse", "perfect", "animals", "creepin"]
     }
 }
