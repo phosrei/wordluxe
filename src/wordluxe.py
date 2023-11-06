c6f77fc53488917b12ece7b2a20c2a4fc5f3451a
Reiki <phosmiro@gmail.com>
2023-11-06 20:22:29 +0800

    Clean code

--- src/wordluxe.py
+++ src/wordluxe.py
@@ -1,3 +1,4 @@
+from string import ascii_lowercase
 import random
 import pycountry
 from wordbank import categories
@@ -30,7 +31,7 @@ def calculate_reward(attempt, max_attempts):
         return 10
     elif max_attempts == 4:
         return 5
-    if attempt <= 2:
+    elif attempt <= 2:
         return 3
     elif attempt <= 4:
         return 2
@@ -51,17 +52,13 @@ def play_game(word, category, max_attempts):
         print(f"Attempt #{attempt}")
         guess = input()
 
-        if guess in ["p", "P"]:
+        if guess.lower() == "p":
             if max_attempts == 3:
                 print("Power-ups are disabled in extreme mode")
-                continue
-            elif currency <= 0:
-                print("Not enough coins")
-                continue
             else:
                 powerup_result = get_powerup(word, output, currency)
-                continue
-            
+            continue
+
         if category not in ["songs", "artists", "sports", "countries"]:
             if guess.lower() not in dictionary:
                 print(f"'{guess}' is not a word.")
@@ -74,7 +71,7 @@ def play_game(word, category, max_attempts):
             print(f"Guess should be {len(word)} letters.")
             continue
 
-        output = (check_guess(guess, word))
+        output = check_guess(guess, word)
         print(output)
 
         if powerup_result == "2":
@@ -119,12 +116,14 @@ def check_guess(guess, word):
     return ''.join(output)
 
 def get_powerup(word, output, currency):
-    while True:
-        powerup_input = input("Choose a power-up:\n"
-        "1. Letter Eraser (-1 coin)\n"
-        "2. Invincibility (-2 coins)\n"
-        "3. Reveal Vowels (-3 coins)\n").upper()
+    print("Choose a power-up:")
+    print("1. Letter Eraser (-1 coin)")
+    print("2. Invincibility (-2 coins)")
+    print("3. Reveal Vowels (-3 coins)")
 
+    powerup_input = input().upper()
+
+    if powerup_input in ["Q", "1", "2", "3"]:
         if powerup_input == "Q":
             return
         elif powerup_input == "1" and currency >= 1:
@@ -137,30 +136,32 @@ def get_powerup(word, output, currency):
             currency -= 3
             vowel_powerup(word)
         else:
-            print("Invalid input")
+            print("Not enough coins")
+    else:
+        print("Invalid input")
+    return
 
 def vowel_powerup(word):
-  vowels = 'aeiou'
-  vowels_hint = []
+    vowels = 'aeiou'
+    vowels_hint = []
 
-  for i in range(len(word)):
-    if word[i] in vowels:
-      if word[i] not in vowels_hint:
-        vowels_hint.append(word[i])
+    for char in word:
+        if char in vowels and char not in vowels_hint:
+            vowels_hint.append(char)
 
     vowel_string = ", ".join(vowels_hint)
-  print(f"The vowel(s) in the word is/are: {vowel_string}")
+    print(f"The vowel(s) in the word is/are: {vowel_string.upper()}")
 
 def eraser_powerup(guess, word):
-    eraser = "abcdefghijklmnopqrstvwxyz"
+    alphabet = ascii_lowercase
     eraser_list = []
 
-    for i in range(len(eraser)):
-        if eraser[i] not in word and eraser[i] not in guess:
-                eraser_list.append(eraser[i])
+    for letter in alphabet:
+        if letter not in word and letter not in guess:
+                eraser_list.append(letter)
 
-    random_unused_letter = random.choice(list(eraser_list)).upper()
-    print(f"{random_unused_letter} is not in the word")
+    random_unused_letter = random.choice(eraser_list).upper()
+    print(f"{random_unused_letter.upper()} is not in the word")
 
 def easy_mode(word, category):
     play_game(word, category, max_attempts = float("inf"))
@@ -176,16 +177,17 @@ def extreme_mode(word, category):
     play_game(word, category, max_attempts = 3)
 
 def end_options(word, category, max_attempts):
-    end_input = input("RETRY (R) -- NEW GAME (N) -- QUIT (Q) ").upper()
-    if end_input == "R":
-        play_game(word, category, max_attempts)
-    elif end_input == "N":
-        main()
-    elif end_input == "Q":
-        return
-    else:
-        print("Invalid input")
-        end_options(word, category, max_attempts)
+    while True:
+        end_input = input("RETRY (R) -- NEW GAME (N) -- QUIT (Q) ").upper()
+        
+        if end_input == "R":
+            play_game(word, category, max_attempts)
+        elif end_input == "N":
+            main()
+        elif end_input == "Q":
+            return
+        else:
+            print("Invalid input")
 
 def main():
     cat_input = validate_input("Choose category: ", categories)
