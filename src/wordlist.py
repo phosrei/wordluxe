60776bd2f200923793d38f26244aa693a7f03907
endevium <148771728+endevium@users.noreply.github.com>
2023-11-30 09:18:26 +0800

    Remove words in countries category

--- src/wordlist.py
+++ src/wordlist.py
@@ -1,7 +1,7 @@
 categories = {
     "general": {
         "easy": {"here", "book", "cake", "rain", "bird", "fire", "fish", "game", "jump", "kind", "loud", "moon", "nest", "open", "park", "quit", "rest", "star", "time", "view"},
-        "normal": {"beach", "chair", "dance", "early", "fence", "glass", "happy", "igloo", "jumbo", "kite", "music", "nurse", "ocean", "piano", "quick", "river", "smile", "table"},
+        "normal": {"apple"},
         "hard": {"bright", "yellow", "purple", "hidden", "flavor", "dinner", "turtle", "puzzle", "rabbit", "guitar", "purple", "floral", "market", "window", "summer", "people", "planet", "friend", "invent"},
         "extreme": {"chicken", "musical", "capital", "journey", "freedom", "unknown", "honesty", "victory", "holiday", "history", "hydrate", "illegal", "silence", "justify", "monster", "breathe", "perfect", "nuclear", "quality", "society"}
     },
@@ -9,7 +9,7 @@ categories = {
         "easy": {"chad", "peru", "oman", "cuba", "togo", "fiji", "mali", "iraq"},
         "normal": {"egypt", "ghana", "india", "japan", "kenya", "libya", "nepal", "qatar", "spain", "turkey", "yemen", "chile", "bhutan", "haiti", "italy", "libya", "niger", "sudan"},
         "hard": {"brazil", "canada", "france", "greece", "jordan", "kuwait", "norway", "poland", "russia", "sweden", "turkey"},
-        "extreme": {"andorra", "germany", "austria", "bahrain", "belgium", "bolivia", "comoros", "croatia", "eritrea", "estonia", "finland", "georgia", "hungary", "iceland", "lesotho", "moldova", "namibia"}
+        "extreme": {"andorra", "germany", "austria", "bahrain", "belgium", "comoros", "croatia", "eritrea", "estonia", "finland", "georgia", "hungary", "iceland", "lesotho", "namibia"}
     },
     "animals": {
         "easy": {"frog", "duck", "goat", "fish", "crab", "lion", "puma", "hawk", "mule", "hare", "toad", "mole", "lamb"},
