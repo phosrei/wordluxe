b4e2302ca727d4165bb8f3f6b5d1c285f3f4af36
endevium <148771728+endevium@users.noreply.github.com>
2023-11-30 11:19:22 +0800

    Create artists_list.py

---
+++ src/artists_list.py
@@ -0,0 +1,6 @@
+artists_list = [
+    "abba", "pink", "enya", "zedd", "muse", "kygo", "cher", "niki", "lorde", "elvis",
+    "adele", "usher", "miley", "elton", "twice", "avril", "halsey", "alicia", "olivia",
+    "eminem", "mariah", "shakira", "ariana", "taylor", "willow", "vanessa", "madonna",
+    "freddie", "beyonce", "pitbull", "beatles", "shakira", "whitney", "rihanna", "nirvana", "britney"
+]
